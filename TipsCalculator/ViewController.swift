cfb34cc680f155134f606d655b64de13ed5afd84
Arturo Fernandez <aferathlon@gmail.com>
2016-09-20 20:53:53 -0700

    Make keyboard first responder as soon as it appears

--- TipsCalculator/ViewController.swift
+++ TipsCalculator/ViewController.swift
@@ -41,6 +41,7 @@ class ViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        totalConsumeLabel?.becomeFirstResponder()
         
     }
     
