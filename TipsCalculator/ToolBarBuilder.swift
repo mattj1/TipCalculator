17f5a7b70adef10c900520e591885b408c22d226
Arturo Fernandez <aferathlon@gmail.com>
2016-09-28 16:31:08 -0700

    Included a responder as a parameter

--- TipsCalculator/ToolBarBuilder.swift
+++ TipsCalculator/ToolBarBuilder.swift
@@ -10,12 +10,12 @@ import Foundation
 import UIKit
 
 class ToolBarBuilder: NSObject {
-    class func addToolBar(textView: UITextView, tempSelector: Selector, title: String) {
+    class func addToolBar(textView: UITextView, tempSelector: Selector, title: String, responder: NSObject) {
         let toolBar = UIToolbar()
         toolBar.barStyle = UIBarStyle.default
         toolBar.isTranslucent = true
         toolBar.tintColor = UIColor.blue
-        let doneButton:UIBarButtonItem = UIBarButtonItem(title: title, style: UIBarButtonItemStyle.done, target: self, action: tempSelector)
+        let doneButton:UIBarButtonItem = UIBarButtonItem(title: title, style: UIBarButtonItemStyle.done, target: responder, action: tempSelector)
         let spaceButton:UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
         toolBar.setItems([spaceButton, doneButton], animated: false)
         toolBar.isUserInteractionEnabled = true
